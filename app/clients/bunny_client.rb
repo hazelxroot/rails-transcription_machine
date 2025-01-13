7e53fa9965ac9217c0e28eba6db2d0e97c8b0054
hazelxroot <samh349@gmail.com>
2025-01-12 23:18:08 -0500

    Sync videos with API

---
+++ app/clients/bunny_client.rb
@@ -0,0 +1,47 @@
+require "net/http"
+
+class BunnyClient
+  BASE_URI = "https://video.bunnycdn.com"
+
+  def initialize(library_id:, access_key:)
+    @library_id, @access_key = library_id, access_key
+  end
+
+  def videos(page: 1, per_page: 1000)
+    get "/videos?itemsPerPage=#{per_page}&page=#{page}"
+  end
+
+  def get(path)
+    http_request Net::HTTP::Get, "/library/#{@library_id}#{path}"
+  end
+
+  def post
+    http_request Net::HTTP::Post, "/library/#{@library_id}#{path}", body: body
+  end
+
+  private
+
+  def http_request(method, path, body: nil)
+    uri = URI.parse("#{BASE_URI}#{path}")
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.is_a?(URI::HTTPS)
+
+    request = method.new(uri)
+    request["accept"] = "application/json"
+    request["AccessKey"] = @access_key
+
+    if request.is_a? Net::HTTP::Post
+      request.body = body.to_json
+      request["Content-Type"] = "application/json"
+    end
+
+    response = http.request(request)
+    if response.is_a? Net::HTTPSuccess
+      JSON.parse(response.body).with_indifferent_access
+    else
+      binding.irb
+      raise StandardError, "Request failed"
+    end
+  end
+end
