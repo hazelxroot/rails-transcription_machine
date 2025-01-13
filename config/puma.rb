172412aed469f79a71416edc99960b1b3eb4bc05
hazelxroot <samh349@gmail.com>
2025-01-12 22:37:34 -0500

    Create database model

--- config/puma.rb
+++ config/puma.rb
@@ -28,6 +28,7 @@ port ENV.fetch("PORT", 3000)
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+plugin :tailwindcss if Rails.env.local?
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
